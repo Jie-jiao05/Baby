ecd8662c385b66cfc82820cf6a8b00d1a693348a
Jie-jiao05 <jiejiao404@gmail.com>
2025-10-15 23:25:59 -0400

    Add files via upload

--- cleaning.R
+++ cleaning.R
@@ -19,3 +19,22 @@ fert_clean <- fert_clean %>%
 head(fert_clean)
 
 write_csv(fert_clean, here("Cleaned_fert_2023.csv"))
+
+# #######
+fert_clean <- fert %>%
+  select(`Country Name`, `1960`, `2023`) %>%
+  filter(
+    !is.na(`1960`),
+    !is.na(`2023`)
+  ) %>%
+  filter(!grepl("World|Income|Region", `Country Name`, ignore.case = TRUE)) %>%
+  rename(
+    Country = `Country Name`,
+    FertilityRate_1960 = `1960`,
+    FertilityRate_2023 = `2023`
+  )
+
+# --- Step 3: Preview and save ---
+head(fert_clean)
+
+write_csv(fert_clean, here("Cleaned_fert_1960_2023.csv"))
